453dfdd675d6fc671f892646ccfe742d43e7ec6e
S. Priemshiy <s.priemshiy@gmail.com>
2024-05-25 09:21:32 +0500

    first

--- inca/main.py
+++ inca/main.py
@@ -7,22 +7,46 @@ import writer as wf
 # Shows in the log when the app starts
 print("Hello world!")
 
+
 # Its name starts with _, so this function won't be exposed
 def _update_message(state):
     is_even = state["counter"] % 2 == 0
     message = ("+Even" if is_even else "-Odd")
     state["message"] = message
 
+
 def decrement(state):
     state["counter"] -= 1
     _update_message(state)
+
 
 def increment(state):
     state["counter"] += 1
     # Shows in the log when the event handler is run
     print("The counter has been incremented.")
     _update_message(state)
-    
+
+
+def to_usd(state):
+    state["currency"] = "USD"
+    print("Currency changed to USD")
+
+
+def to_eur(state):
+    state["currency"] = "EUR"
+    print("Currency changed to EUR")
+
+
+def to_uah(state):
+    state["currency"] = "UAH"
+    print("Currency changed to UAH")
+
+
+def to_sum(state):
+    state["currency"] = "SUM"
+    print("Currency changed to SUM")
+
+
 # Initialise the state
 
 # "_my_private_element" won't be serialised or sent to the frontend,
